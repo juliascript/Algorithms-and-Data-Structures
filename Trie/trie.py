bc3595a4ce2a8224bb24e0802b611893c0e4d61f
Julia <juliageist23@gmail.com>
2016-11-06 16:55:43 -0800

    removed function that is incomplete

--- Trie/trie.py
+++ Trie/trie.py
@@ -21,31 +21,9 @@ def isWordPresentInTrie(trie, word):
 	else: 
 		return False 
 
-def offerPossibleCompletionsToStringInTrie(trie, string):
-	currentDict = trie
-	arrayOfPossibleWords = []
-	for letter in string: 
-		if letter in currentDict:
-			currentDict = currentDict[letter]
-		else:
-			return "Trie does not offer any completions to %s" % (string)
-	# now current dict is at the level which would offer completions. 
-	# at this point, it is necessary to step down through all possiblities 
-	# grab the values at this level, count them, and then for that many times, go through into the last step. 
-	# if there, at any time is more than one value per key (there is more than one possible completion),
-	#   that should also be followed, and likewise for following that, if there are other paths, follow them
-	# if at any point, do you find the endOfWord, add the path to the array as a string
-
-	for nextLetter in currentDict:
-		print nextLetter
-
-	
-
-
 
 arrayOfWords = ['hello', 'hey', 'what', 'when', 'why']
 wordsTrie = generateTrieFromWordsArray(arrayOfWords)
-# print wordsTrie['h']['e']
-# print isWordPresentInTrie(wordsTrie, 'hello')
-# print isWordPresentInTrie(wordsTrie, 'hellow')
-print offerPossibleCompletionsToStringInTrie(wordsTrie, 'he')
+ print wordsTrie
+ print isWordPresentInTrie(wordsTrie, 'hello')
+ print isWordPresentInTrie(wordsTrie, 'hellow')
